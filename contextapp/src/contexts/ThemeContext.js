3c33e640e1b4ee5ecc4d9c7ceb5d10dc3264337b
Tiana Johnson <tiana.marie013@gmail.com>
2019-12-27 10:27:21 -0600

    </Themed...>

--- contextapp/src/contexts/ThemeContext.js
+++ contextapp/src/contexts/ThemeContext.js
@@ -1,13 +1,20 @@
 import React, {createContext } from 'react';
-import { connect } from 'react-redux';
+
+export const ThemeContext = createContext();
+
 class ThemeContextProvider extends Component {
+    state = {
+        isLightTheme: true,
+        light: { syntax: '#555', ui: '#ddd', bg: '#eee'},
+        dark: { syntax: '#ddd', ui: '#333', bg: '#555'}
+    }
     render() {
         return (
-            <div></div>
-         )
+            <ThemeContext.Provider value={{...}}>
+
+            </ThemeContext.Provider>
+         );
     }
 }
-const mapStateToProps = reduxStore => {
-    return { reduxStore: reduxStore };
-    }
-export default connect(mapStateToProps)(ThemeContextProvider);
+
+export default ThemeContextProvider;
